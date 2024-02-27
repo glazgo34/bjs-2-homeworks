ff764c4c8272c2b5a2dbb8f040caa8f0837d146d
glazgo34 <glazyrinilya2011@gmail.com>
2024-02-27 15:57:00 +0500

    task 1 done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = b**2 - 4 * a * c;
+	if (D < 0) {
+		return [];
+	} else if (D === 0) {
+		console.log(-b/(2*a));
+		return [-b/(2*a)];
+	} 
+	
+	arr.push((-b + Math.sqrt(D))/(2 * a));
+	arr.push((-b - Math.sqrt(D))/(2 * a));
   return arr;
 }
+
+let uravnenie = solveEquation(1, 5, -3);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
