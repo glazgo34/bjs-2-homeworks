875145b1601d0d15d65430178a180bd1b71b46dc
glazgo34 <glazyrinilya2011@gmail.com>
2024-02-29 16:39:22 +0500

    array tasks done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+  if(arr1.length === arr2.length) {
+		if(arr1.every((item, inx) => item === arr2[inx])) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let person = users.filter((item) => item.gender === gender).map((item) => item.age);
+	let result = person.reduce((acc, item) => acc + item, 0);
+
+	if(person.length === 0 ) {
+		return 0;
+	}
+ 
+	return result/person.length;
 }
