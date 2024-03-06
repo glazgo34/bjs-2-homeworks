c3532e5ed93f211394d39b3ce240d39e65ac4e7f
glazgo34 <glazyrinilya2011@gmail.com>
2024-03-06 18:16:36 +0500

    Задания выполнены

---
+++ 7.async/task.js
@@ -0,0 +1,57 @@
+class AlarmClock {
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
+
+	addClock(time, callback) {
+		if (!time || !callback) {
+			throw new Error("Отсутствуют обязательные аргументы");
+		}
+		if (this.alarmCollection.find(clock => clock.time === time)) {
+			console.warn("Уже присутствует звонок на это же время");
+		}
+		return this.alarmCollection.push({
+			callback,
+			time,
+			canCall: true
+		});
+	}
+
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter((item) => item.time !== time);
+	}
+
+	getCurrentFormattedTime() {
+        return new Date().toTimeString().slice(0, 5);
+    }
+
+	start() {
+		if (this.intervalId) {
+			return;
+		}
+		this.intervalId = setInterval(() => {
+			this.alarmCollection.forEach((element) => {
+				if (this.getCurrentFormattedTime() === element.time) {
+					element.canCall = false;
+					element.callback();
+				}
+			})
+		}, 1000);
+	}
+
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
+
+	resetAllCalls() {
+		this.alarmCollection.forEach((item) => item.canCall = true);
+	}
+
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
+}
+
